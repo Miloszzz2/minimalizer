9c80bf9aed74cb1a54afb3a40c3d798fa9169472
Miloszzz2 <97192271+Miloszzz2@users.noreply.github.com>
2024-05-25 22:30:10 +0200

    added main page recommendations hiding

--- src/background/index.ts
+++ src/background/index.ts
@@ -1,79 +1,113 @@
+import type { SwitchedChecked } from "../popup";
+
 async function init() {
-    var targetNode = document.body;
+	const targetNode = document.body;
 
-    var config = { childList: true, subtree: true };
-    var callback = async function (mutationsList: any, observer: any) {
-        for (var mutation of mutationsList) {
-            if (mutation.type === 'childList') {
-                const data = await chrome.storage.sync.get("options");
-                const recommendationsEl = document.getElementsByTagName("ytd-watch-next-secondary-results-renderer")[0] as HTMLElement;
-                const shortsEl = document.getElementsByTagName("ytd-reel-shelf-renderer")[0] as HTMLElement;
-                const commentsEl = document.getElementsByTagName("ytd-comments")[0] as HTMLElement;
-                const richShortsRenderer = document.getElementsByTagName("ytd-rich-section-renderer") as HTMLCollectionOf<HTMLElement>;
-                if (shortsEl || recommendationsEl || commentsEl || richShortsRenderer[0]) {
-                    if (data.options)
-                        VisibilityChange(data.options);
+	const config = { childList: true, subtree: true };
+	const callback = async (
+		mutationsList: MutationRecord[],
+		observer: MutationObserver,
+	) => {
+		for (const mutation of mutationsList) {
+			if (mutation.type === "childList") {
+				const data = await chrome.storage.sync.get("options");
+				const recommendationsEl = document.getElementsByTagName(
+					"ytd-watch-next-secondary-results-renderer",
+				)[0] as HTMLElement;
+				const shortsEl = document.getElementsByTagName(
+					"ytd-reel-shelf-renderer",
+				)[0] as HTMLElement;
+				const commentsEl = document.getElementsByTagName(
+					"ytd-comments",
+				)[0] as HTMLElement;
+				const richShortsRenderer = document.getElementsByTagName(
+					"ytd-rich-section-renderer",
+				) as HTMLCollectionOf<HTMLElement>;
+				const richGridRenderer = document.querySelectorAll(
+					"#contents.ytd-rich-grid-renderer",
+				)[0] as HTMLElement;
+				if (
+					shortsEl ||
+					recommendationsEl ||
+					commentsEl ||
+					richShortsRenderer[0] ||
+					richGridRenderer
+				) {
+					if (data.options) VisibilityChange(data.options);
 
-                    if (data.options.shorts_checked === true) {
-                        console.log("stopped1")
-                        observer.disconnect();
-                        return;
-                    }
-                }
+					if (data.options.shorts_checked === true) {
+						console.log("stopped1");
+						observer.disconnect();
+						return;
+					}
+				}
+			}
+		}
+	};
 
-            }
-        }
-    };
+	const observer = new MutationObserver(callback);
 
-    var observer = new MutationObserver(callback);
-
-    observer.observe(targetNode, config);
+	observer.observe(targetNode, config);
 }
 
 init();
 
-chrome.storage.onChanged.addListener(
-    function (changes, area) {
-        if (area == 'sync' && changes.options?.newValue) {
-            VisibilityChange(changes.options.newValue)
-        }
-    }
-);
-function VisibilityChange(changes: any) {
-    const recommendationsEl = document.getElementsByTagName("ytd-watch-next-secondary-results-renderer")[0] as HTMLElement;
-    const reel_shorts_renderer = document.getElementsByTagName("ytd-reel-shelf-renderer") as HTMLCollectionOf<HTMLElement>;
-    const rich_shorts_renderer = document.getElementsByTagName("ytd-rich-section-renderer") as HTMLCollectionOf<HTMLElement>;
-    const shorts = Array.from(reel_shorts_renderer).concat(Array.from(rich_shorts_renderer));
-    const commentsEl = document.getElementsByTagName("ytd-comments")[0] as HTMLElement;
+chrome.storage.onChanged.addListener((changes, area) => {
+	if (area === "sync" && changes.options?.newValue) {
+		VisibilityChange(changes.options.newValue);
+	}
+});
+function VisibilityChange(changes: SwitchedChecked) {
+	const recommendationsEl = document.getElementsByTagName(
+		"ytd-watch-next-secondary-results-renderer",
+	)[0] as HTMLElement;
+	const reel_shorts_renderer = document.getElementsByTagName(
+		"ytd-reel-shelf-renderer",
+	) as HTMLCollectionOf<HTMLElement>;
+	const rich_shorts_renderer = document.getElementsByTagName(
+		"ytd-rich-section-renderer",
+	) as HTMLCollectionOf<HTMLElement>;
+	const shorts = Array.from(reel_shorts_renderer).concat(
+		Array.from(rich_shorts_renderer),
+	);
+	const commentsEl = document.getElementsByTagName(
+		"ytd-comments",
+	)[0] as HTMLElement;
+	const main_page_recommends = document.querySelectorAll(
+		"#contents.ytd-rich-grid-renderer",
+	)[0] as HTMLElement;
+	if (recommendationsEl) {
+		if (Boolean(changes.recommendations_checked) === false) {
+			recommendationsEl.style.display = "none";
+		} else {
+			recommendationsEl.style.display = "flex";
+		}
+	}
 
-    if (recommendationsEl) {
-        if (Boolean(changes.recommendations_checked) === false) {
-            recommendationsEl.style.display = "none";
-        }
-        else {
-            recommendationsEl.style.display = "flex";
-        }
-    }
+	if (shorts.length > 0) {
+		if (Boolean(changes.shorts_checked) === false) {
+			for (const shorts_obj of shorts) {
+				shorts_obj.style.display = "none";
+			}
+		} else {
+			for (const shorts_obj of shorts) {
+				shorts_obj.style.display = "flex";
+			}
+		}
+	}
 
-    if (shorts.length > 0) {
-        if (Boolean(changes.shorts_checked) === false) {
-            shorts.forEach((shorts_obj) => {
-                shorts_obj.style.display = "none"
-            });
-        } else {
-            Array.from(shorts).forEach((shorts_obj) => {
-                shorts_obj.style.display = "flex"
-            });
-        }
-    }
-
-
-    if (commentsEl) {
-        if (Boolean(changes.comments_checked) === false) {
-            commentsEl.style.display = "none";
-        }
-        else {
-            commentsEl.style.display = "flex";
-        }
-    }
+	if (commentsEl) {
+		if (Boolean(changes.comments_checked) === false) {
+			commentsEl.style.display = "none";
+		} else {
+			commentsEl.style.display = "flex";
+		}
+	}
+	if (main_page_recommends) {
+		if (Boolean(changes.main_checked) === false) {
+			main_page_recommends.style.display = "none";
+		} else {
+			main_page_recommends.style.display = "flex";
+		}
+	}
 }
--- src/popup/index.ts
+++ src/popup/index.ts
@@ -1,54 +1,77 @@
-import './output.css';
+import "./output.css";
 
-const shorts_switch = document.getElementById('shorts-content') as HTMLInputElement;
-const recommendations_switch = document.getElementById('recommends-content') as HTMLInputElement;
-const comments_switch = document.getElementById('comments-content') as HTMLInputElement;
+const shorts_switch = document.getElementById(
+	"shorts-content",
+) as HTMLInputElement;
+const recommendations_switch = document.getElementById(
+	"recommends-content",
+) as HTMLInputElement;
+const comments_switch = document.getElementById(
+	"comments-content",
+) as HTMLInputElement;
+const main_switch = document.getElementById("main-content") as HTMLInputElement;
 
-type SwitchedChecked = {
-    shorts_checked: boolean,
-    recommendations_checked: boolean,
-    comments_checked: boolean
-}
+export type SwitchedChecked = {
+	shorts_checked: boolean;
+	recommendations_checked: boolean;
+	comments_checked: boolean;
+	main_checked: boolean;
+};
 
 let options: SwitchedChecked = {
-    shorts_checked: shorts_switch.checked,
-    recommendations_checked: recommendations_switch.checked,
-    comments_checked: comments_switch.checked
-}
+	shorts_checked: shorts_switch.checked,
+	recommendations_checked: recommendations_switch.checked,
+	comments_checked: comments_switch.checked,
+	main_checked: main_switch.checked,
+};
 
 async function LoadData() {
-    const data = await chrome.storage.sync.get("options");
-    Object.assign(options, data.options);
-    shorts_switch.checked = Boolean(options.shorts_checked);
-    recommendations_switch.checked = Boolean(options.recommendations_checked);
-    comments_switch.checked = Boolean(options.comments_checked);
+	const data = await chrome.storage.sync.get("options");
+	Object.assign(options, data.options);
+	shorts_switch.checked = Boolean(options.shorts_checked);
+	recommendations_switch.checked = Boolean(options.recommendations_checked);
+	comments_switch.checked = Boolean(options.comments_checked);
+	main_switch.checked = Boolean(options.main_checked);
 }
 
 LoadData();
 
-shorts_switch.addEventListener('change', () => {
-    options = {
-        shorts_checked: shorts_switch.checked,
-        recommendations_checked: recommendations_switch.checked,
-        comments_checked: comments_switch.checked
-    }
-    chrome.storage.sync.set({ options })
+shorts_switch.addEventListener("change", () => {
+	options = {
+		shorts_checked: shorts_switch.checked,
+		recommendations_checked: recommendations_switch.checked,
+		comments_checked: comments_switch.checked,
+		main_checked: main_switch.checked,
+	};
+	chrome.storage.sync.set({ options });
 });
 
-recommendations_switch.addEventListener('change', () => {
-    options = {
-        shorts_checked: shorts_switch.checked,
-        recommendations_checked: recommendations_switch.checked,
-        comments_checked: comments_switch.checked
-    }
-    chrome.storage.sync.set({ options })
+recommendations_switch.addEventListener("change", () => {
+	options = {
+		shorts_checked: shorts_switch.checked,
+		recommendations_checked: recommendations_switch.checked,
+		comments_checked: comments_switch.checked,
+		main_checked: main_switch.checked,
+	};
+	chrome.storage.sync.set({ options });
 });
 
-comments_switch.addEventListener('change', () => {
-    options = {
-        shorts_checked: shorts_switch.checked,
-        recommendations_checked: recommendations_switch.checked,
-        comments_checked: comments_switch.checked
-    }
-    chrome.storage.sync.set({ options })
+comments_switch.addEventListener("change", () => {
+	options = {
+		shorts_checked: shorts_switch.checked,
+		recommendations_checked: recommendations_switch.checked,
+		comments_checked: comments_switch.checked,
+		main_checked: main_switch.checked,
+	};
+	chrome.storage.sync.set({ options });
+});
+
+main_switch.addEventListener("change", () => {
+	options = {
+		shorts_checked: shorts_switch.checked,
+		recommendations_checked: recommendations_switch.checked,
+		comments_checked: comments_switch.checked,
+		main_checked: main_switch.checked,
+	};
+	chrome.storage.sync.set({ options });
 });
